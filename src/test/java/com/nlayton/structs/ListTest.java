cbc6faea9607ce374a41677f3c75932a85eacb87
nicholas.layton <nicholas.layton@workday.com>
2019-11-11 17:07:53 -0800

    Working on tests

--- src/test/java/com/nlayton/structs/ListTest.java
+++ src/test/java/com/nlayton/structs/ListTest.java
@@ -3,7 +3,9 @@ package com.nlayton.structs;
 import static com.nlayton.NickMatchers.isFalse;
 import static com.nlayton.NickMatchers.isTrue;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.arrayContaining;
 import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.fail;
 
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +26,6 @@ public class ListTest {
 
     @Rule
     public final TestName testName = new TestName();
-
     private List<Integer> list;
 
     public ListTest(final List<Integer> list) {
@@ -62,85 +63,119 @@ public class ListTest {
         assertThat(list.contains(1), isFalse());
         list.add(1);
         assertThat(list.contains(1), isTrue());
+        list.remove(1);
+        assertThat(list.contains(1), isFalse());
     }
 
     @Test
     public void iterator() {
+        fail();
     }
 
     @Test
     public void toArray() {
+        list.addAll(Arrays.asList(1, 2, 3, 4, 5));
+        final Object[] a = list.toArray();
+        assertThat(a, arrayContaining(1, 2, 3, 4, 5));
     }
 
     @Test
     public void testToArray() {
+        list.addAll(Arrays.asList(5,6,7,8,9));
+        final int[] a = list.toArray(new int[]);
+        assertThat(a, arrayContaining(5,6,7,8,9));
     }
 
     @Test
     public void add() {
+        assertThat(list.add(1), isTrue());
+        assertThat(list.size(), is(1));
+        assertThat(list.add(1), isTrue());
+        assertThat(list.size(), is(2));
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void addNull() {
+        list.add(null);
     }
 
     @Test
     public void remove() {
+        fail();
     }
 
     @Test
     public void containsAll() {
+        fail();
     }
 
     @Test
     public void addAll() {
+        fail();
     }
 
     @Test
     public void testAddAll() {
+        fail();
     }
 
     @Test
     public void removeAll() {
+        fail();
     }
 
     @Test
     public void retainAll() {
+        fail();
     }
 
     @Test
     public void clear() {
+        fail();
     }
 
     @Test
     public void get() {
+        fail();
     }
 
     @Test
     public void set() {
+        fail();
     }
 
     @Test
     public void testAdd() {
+        fail();
     }
 
     @Test
     public void testRemove() {
+        fail();
     }
 
     @Test
     public void indexOf() {
+        fail();
     }
 
     @Test
     public void lastIndexOf() {
+        fail();
     }
 
     @Test
     public void listIterator() {
+        fail();
     }
 
     @Test
     public void testListIterator() {
+        fail();
     }
 
     @Test
     public void subList() {
+        fail();
     }
 }
